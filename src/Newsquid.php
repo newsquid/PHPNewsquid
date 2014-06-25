80a46409fd30ab2bb51258a94f09c1f40f434ed7
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2014-06-25 15:27:38 +0200

    corrected arguments for Newsquid

--- src/Newsquid.php
+++ src/Newsquid.php
@@ -5,12 +5,13 @@ class Newsquid {
     private $newsquid_url;
     private $application_url;
 
-    public function __construct($newsquid_url, $application_url) {
+    public function __construct($newsquid_url, $uid, $secret) {
         $this->newsquid_url = $newsquid_url;
-        $this->application_url = $application_url;
+        $this->uid = $uid;
+        $this->secret = $secret;
     }
 
-    public function getArticles($user = null) {
+    public function getArticles($user_token) {
         throw new Exception("Not implemented");
     }
 
--- test/NewsquidTest.php
+++ test/NewsquidTest.php
@@ -1,9 +1,14 @@
 <?php
 
+/**
+ * This test is pretty much an integration test, from top to bottom,
+ * and as such requires a fake backend to exist.
+ * This version relies on nsqor to be running on :1337 
+ */
 class NewsquidTest extends PHPUnit_Framework_TestCase {
     
     public function testGetAllUsers() {
-        $nsq = new Newsquid("http://payment.nsquid.co", null);
+        $nsq = new Newsquid("https://localhost:1337", "uid_test", "secret_test");
         
         $nsq->getUsers();
     }
