dd2db5cb4b8b3642106ec177f57778822caea9b4
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2014-06-27 16:49:03 +0200

    Integration test of already-bought article

--- test/integration/NewsquidUserIntegrationTest.php
+++ test/integration/NewsquidUserIntegrationTest.php
@@ -4,7 +4,7 @@ class NewsquidUserIntegrationTest extends PHPUnit_Framework_TestCase {
 
     private $reader_one, $writer_one;
     private $newsquid_caller;
-    private $product_one;
+    private $product_one, $product_bought_by_one;
 
     public function setUp() {
         $this->newsquid_caller = new CurlRemoteCaller("https://localhost:1337/api/v2");
@@ -17,12 +17,25 @@ class NewsquidUserIntegrationTest extends PHPUnit_Framework_TestCase {
 
         //Product owned by writer_one
         $this->product_one = new NewsquidProduct(1, "product_one", 11, "USD", "url_one", $this->newsquid_caller);
+        $this->product_bought_by_one = new NewsquidProduct(5, "product_title", 1.1, "USD", "bought_url", $this->newsquid_caller);
     }
 
-    public function test_NewsquidUser_CanAccessProduct() {
+    public function test_NewsquidUser_CanAccessProduct_Fail() {
         $r1_can_access = $this->reader_one->canAccessProduct($this->product_one);
 
         $this->assertFalse($r1_can_access);
+    }
+
+    public function test_NewsquidUser_CanAccessProduct_OwnerSuccess() {
+        $w1_can_access = $this->writer_one->canAccessProduct($this->product_one);
+
+        $this->assertTrue($w1_can_access);
+    }
+
+    public function test_NewsquiUser_CanAccessProduct_BoughtSuccess() {
+        $r1_can_access = $this->reader_one->canAccessProduct($this->product_bought_by_one);
+
+        $this->assertTrue($r1_can_access);
     }
 
 }
