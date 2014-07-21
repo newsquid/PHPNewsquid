75a10d48f72eddfb542874b5652f61123d53468c
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2014-07-21 11:42:56 +0200

    buyProduct integration test

--- test/integration/NewsquidUserIntegrationTest.php
+++ test/integration/NewsquidUserIntegrationTest.php
@@ -5,13 +5,13 @@ class NewsquidUserIntegrationTest extends PHPUnit_Framework_TestCase {
     private $reader_one, $writer_one;
     private $newsquid_caller;
     private $product_one, $product_bought_by_one;
+    private $newsquid;
 
     public function setUp() {
         $this->newsquid_caller = new CurlRemoteCaller("https://localhost:1337");
-        $this->newsquid_caller->insecure = true;
-        $this->newsquid_caller->user = "uid_test";
-        $this->newsquid_caller->password = "secret_test";
-        
+
+        $this->newsquid = new Newsquid($this->newsquid_caller, "uid_test", "secret_test", true);
+
         $this->reader_one = new NewsquidUser(1, "reader_one", "reader_one@trunktrunk.org", "50c9ff9c351783ef57a1a85ae36c0537cee49bfaf409bd785721a8e2e0207c77", $this->newsquid_caller);
         $this->writer_one = new NewsquidUser(2, "writer_one", "writer_one@trunktrunk.org", "40c9ff9c351783ef57a1a85ae36c0537cee49bfaf409bd785721a8e2e0207c77", $this->newsquid_caller);
 
@@ -36,6 +36,20 @@ class NewsquidUserIntegrationTest extends PHPUnit_Framework_TestCase {
         $r1_can_access = $this->reader_one->canAccessProduct($this->product_bought_by_one);
 
         $this->assertTrue($r1_can_access);
+    }
+
+    public function test_NewsquiUser_BuyProduct_VeryfiyAccess() {
+        $prod = $this->newsquid->createProduct(23, "Test Product", 10, "USD", "http://thismyshit.com", $this->writer_one);
+
+        $can_access_before = $this->reader_one->canAccessProduct($prod);
+
+        $this->assertFalse($can_access_before);
+
+        $this->reader_one->buyProduct($prod);
+
+        $can_access_after = $this->reader_one->canAccessProduct($prod);
+
+        $this->assertTrue($can_access_after);
     }
 
 }
